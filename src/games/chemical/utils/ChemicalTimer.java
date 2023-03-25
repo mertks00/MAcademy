8555bede7c765c8b8a5dd5bd98e43bf4a0b430f9
Mert <95344086+mertks00@users.noreply.github.com>
2023-03-25 14:22:06 +0300

    Update ChemicalTimer.java

--- src/games/chemical/utils/ChemicalTimer.java
+++ src/games/chemical/utils/ChemicalTimer.java
@@ -9,20 +9,11 @@ public class ChemicalTimer implements Runnable {
         Author: https://github.com/mertks00
 
     */
-    private volatile boolean running = true;
+
     int longTime = 480;
-
-    public boolean isRunning() {
-        return running;
-    }
-
-    public void stopThread(boolean running) {
-        this.running = running;
-    }
 
     public void run() {
 
-        while (isRunning()) {
             try {
                 for (int i = longTime; i > 0; i--) {
                     ChemicalFrame.time.setText("SÜRE: " + i);
@@ -32,7 +23,7 @@ public class ChemicalTimer implements Runnable {
                 System.out.println("Hata");
             }
             System.exit(0);
-        }
+        
     }
 
 }
